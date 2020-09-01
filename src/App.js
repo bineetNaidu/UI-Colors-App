0cb6a59c9d03cf4bf8489e2d1c30b7c0301eb872
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-01 21:19:05 +0530

    integrated react router with App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import seedColors from "./seedColors";
 import Palette from "./Palette";
 import { generatePalette } from "./colorHelpers";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+export default class App extends Component {
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id);
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <h1>Mini Palettes goes here</h1>}
+          />
+          <Route
+            exact
+            path="/palette/:id"
+            render={(routerProps) => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routerProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
