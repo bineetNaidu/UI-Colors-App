1e5e182a7413a3c4737f3f5b8a559c9c56291e32
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-13 21:27:12 +0530

    add bg svg to paletteList

--- src/styles/PaletteListStyles.jsx
+++ src/styles/PaletteListStyles.jsx
@@ -1,12 +1,19 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#394bad",
+    backgroundImage: `url(${bg})`,
+    overflow: "auto",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -42,7 +49,7 @@ export default {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem",
+      gridGap: "1.4rem",
     },
   },
 };
